d7a0ac78e2b17ba8bd87e20e2486817b7fae6ebd
Andy Pernsteiner <andypern@gmail.com>
2021-03-02 16:34:01 -0800

    made datagen script better

--- scripts/datagen.sh
+++ scripts/datagen.sh
@@ -6,15 +6,27 @@ scale=$1
 dir=$2
 threads=$3
 
-binary=/usr/bin/dsdgen
+binary=/usr/local/bin/dsdgen
 # annoying..
 cd /root
 
 # iterate through each thread.
 
-for t in {1..$threads}; do
+for t in `seq $threads`; do
    echo "kicking off thread $t"
-   $binary -scale $scale -dir $dir -parallel $threads -child $t &
+   $binary -scale $scale -dir $dir -parallel $threads -child $t >/dev/null 2>/dev/null &
+   genpids="$genpids $!"
 done
 
+# wait for the threads to finish
+
+for pid in $genpids; do
+    wait $pid || let "RESULT=1"
+done
+
+if [ "$RESULT" == "1" ]; then
+    exit 1
+fi
+
+echo "all jobs done"
 
